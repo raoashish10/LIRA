267a65ad2f5c58ed83d5d3a89481a5abd02a352b
aayush1204 <aayush.halgekar@gmail.com>
2022-03-02 16:55:44 +0530

    basic frontend done

--- Recommender/urls.py
+++ Recommender/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name="home"),
+    path('results/', views.results , name="results"),
+    path('search/', views.search, name = "search"),
+    path('summary/', views.summary , name = "summary")
     
 ]
--- Recommender/views.py
+++ Recommender/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def results(request):
+    return render(request, 'results.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def summary(request):
+    return render(request, 'summary.html')
